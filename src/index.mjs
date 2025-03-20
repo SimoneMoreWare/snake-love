8402aa89d284ff9425175672e4f17d739c562409
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2025-03-20 22:44:16 +0100

    refactor

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,1 @@
-import drawBoard from './game/board.mjs';
-import { loadFrameImage } from './game/frameImage.mjs';
-
-loadFrameImage().then(() => {
-    drawBoard(); // Disegna il tabellone una volta che l'immagine è stata caricata
-});
-// Disegna il tabellone quando l'immagine è stata caricata
-frameImage.onload = () => {
-    drawBoard();
-};
-
-
-
+console.log("Ciao Mondo!");
