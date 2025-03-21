f306fdf44bba10c8d0dd6a5641b49a574d5b7e0c
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2025-03-21 22:41:15 +0100

    responsive complete

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,4 +1,10 @@
 export const NUM_ROWS = 21;
 export const NUM_COLS = 21;
 export const HEART_EMOJI = '❤️';
-export const CELL_SIZE = '1.5vw';
+// Imposta CELL_SIZE dinamicamente in base alla larghezza dello schermo
+export const CELL_SIZE = window.innerWidth <= 768 || window.innerHeight <= 768 ? '4vw' : '1.7vw';
+
+// Aggiungi un event listener per aggiornare CELL_SIZE quando la finestra viene ridimensionata
+window.addEventListener('resize', () => {
+    CELL_SIZE = window.innerWidth < 768 || window.innerHeight < 768 ? '4vw' : '1.7vw';
+});
